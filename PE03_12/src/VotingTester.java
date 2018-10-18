ccefa1d356bc3bf6b2650a209fb9076e8c46e6bd
RBPEDIIIAL <tbarnabic1@gmail.com>
2018-10-18 09:56:26 -0400

    PE03_12
    Finished PE03_12

--- PE03_12/src/VotingTester.java
+++ PE03_12/src/VotingTester.java
@@ -1,6 +1,6 @@
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-
+import java.util.TimeZone;
 public class VotingTester {
 	
 	public static void main(String args[]) {
@@ -11,7 +11,17 @@ public class VotingTester {
 		test.voteR();
 		//System.out.println( test.count() );
 		
-		Calendar cal = new GregorianCalendar( Locale EDT);
+		Calendar testCal = new GregorianCalendar( 2018, 11, 6);
+		Calendar cal = new GregorianCalendar( TimeZone.getTimeZone("America/New_York") );
+		
+		cal = testCal;
+		//System.out.println(testCal);
+		//System.out.println(cal);
+		
+		if (testCal == cal) {
+			System.out.println( test.count() );
+		}
+	
 	}
 
 }
