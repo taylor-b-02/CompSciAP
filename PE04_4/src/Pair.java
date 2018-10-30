ecbd815635b62dbf0e8e153e6fa43db437ad6927
RBPEDIIIAL <tbarnabic1@gmail.com>
2018-10-30 09:42:59 -0400

    Worked PE04_4
    Completed part of the Pair Class

--- PE04_4/src/Pair.java
+++ PE04_4/src/Pair.java
@@ -1,4 +1,59 @@
+import java.lang.Math;
 
 public class Pair {
+	
+	double first;
+	double second;
+	
+	public Pair( double aFirst, double aSecond) {
+		
+		double first = aFirst;
+		double second = aSecond;
+	
+	}
+	
+	public double getSum() {
+		
+		return (first + second);
+		
+	}
+	
+	public double getDifference() {
+		
+		return (first / second);
+		
+	}
+
+	public double getProduct() {
+	
+		return (first * second);
+	
+	}
+	
+	public double getAvg() {
+		
+		return ( (first + second) / 2.0);
+		
+	}
+
+	public double getDistance() {
+	
+		return (first + second);
+	
+	}
+
+	public double getMax() {
+	
+		return (first + second);
+	
+	}
+
+	public double getMin() {
+	
+		return (first + second);
+	
+	}
+
+
 
 }
