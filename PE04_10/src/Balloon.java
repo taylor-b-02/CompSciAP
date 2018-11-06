96b906c6be0db7461da245dc8c92172c18d0f7b7
RBPEDIIIAL <tbarnabic1@gmail.com>
2018-11-06 10:46:42 -0500

    Created PE04_10

---
+++ PE04_10/src/Balloon.java
@@ -0,0 +1,37 @@
+
+public class Balloon {
+
+	public Balloon() {
+		air = 0;
+		volume = 0;
+		surfaceArea = 0;
+		radius = 0;
+	}
+	
+	public void addAir(double amount) {
+		
+		c
+		
+	}
+	
+	public double getVolume() {
+		
+		
+	}
+	
+	public double getRadius() {
+		
+		
+	}
+	
+	public double getSurfaceArea() {
+		
+		
+	}
+	
+	double air = 0;
+	double volume = 0;
+	double surfaceArea = 0;
+	double radius = 0;
+
+}
