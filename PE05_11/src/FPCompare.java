bdce802702445598a0b52b27e82e9efc7eab61ef
RBPEDIIIAL <tbarnabic1@gmail.com>
2018-11-25 23:14:27 -0500

    Finished PE05_11

--- PE05_11/src/FPCompare.java
+++ PE05_11/src/FPCompare.java
@@ -1,4 +1,26 @@
-
+import java.math.BigDecimal;
 public class FPCompare {
+	
+	public String Compare(double inum1, double inum2) {
+		double rnum1 = round((float)inum1, 2);
+		double rnum2 = round((float)inum2, 2);
+		double diff = Math.pow((rnum1 - rnum2), 2);
+		String output = "";
+		
+		if (rnum1 == rnum2) output = "They are the same when rounded to two decimal places.";
+		else output = "They are different when rounded to two decimal places";
+		
+		if (diff <= 0.01) output += "\n" + "They differ by less than 0.01";
+		else output += "\n" + "They differ by more than 0.01";
+		
+		return output;
+	}
+	
+	public static float round(float d, int decimalPlace) {
+	    BigDecimal bd = new BigDecimal(Float.toString(d));
+	    bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+	    return bd.floatValue();
+	}
+	
 
 }
--- PE05_11/src/FPCompareTester.java
+++ PE05_11/src/FPCompareTester.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
 
 public class FPCompareTester {
 
 	public static void main(String[] args) {
+
+	Scanner in = new Scanner(System.in);
+	FPCompare test = new FPCompare();
+	
+	System.out.println("Enter two floating-point numbers:");
+	System.out.println(test.Compare(in.nextDouble(), in.nextDouble()));
+
 
 	}
 
