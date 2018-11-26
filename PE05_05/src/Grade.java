ff8756d8508b6a0a7ca8bfdc0c3bf0ca2d647d7c
RBPEDIIIAL <tbarnabic1@gmail.com>
2018-11-25 21:10:05 -0500

    Finished PE05_05 with high efficiency

--- PE05_05/src/Grade.java
+++ PE05_05/src/Grade.java
@@ -1,9 +1,53 @@
 
 public class Grade {
 	
-	public String getNumericGrade(String igrade) {
+	public String getNumericGrade(String ilgrade) {
 		
-		if
+		String lgrade = ilgrade;
+		double numGrade = 0;
+		
+		if (lgrade.compareToIgnoreCase("A+") == 0) numGrade = 4;
+		else if (lgrade.compareToIgnoreCase("A") == 0) numGrade = 4;
+		else if (lgrade.compareToIgnoreCase("A-") == 0) numGrade = 3.7;
+		else if (lgrade.compareToIgnoreCase("B+") == 0) numGrade = 3.3;
+		else if (lgrade.compareToIgnoreCase("B") == 0) numGrade = 3;
+		else if (lgrade.compareToIgnoreCase("B-") == 0) numGrade = 2.7;
+		else if (lgrade.compareToIgnoreCase("C+") == 0) numGrade = 2.3;
+		else if (lgrade.compareToIgnoreCase("C") == 0) numGrade = 2;
+		else if (lgrade.compareToIgnoreCase("C-") == 0) numGrade = 1.7;
+		else if (lgrade.compareToIgnoreCase("D+") == 0) numGrade = 1.3;
+		else if (lgrade.compareToIgnoreCase("D") == 0) numGrade = 1;
+		else if (lgrade.compareToIgnoreCase("D-") == 0) numGrade = .7;
+		else numGrade = 0;
+		
+		
+
+		return String.valueOf(numGrade);
 	}
+	
+	public String EgetNumericGrade(String ilgrade) {
+		
+		String l = ilgrade.substring(0,1);
+		String mod = ilgrade.substring(1);
+		double numGrade = 0;
+		
+		if (mod.compareTo("+") == 0) numGrade += .3;
+		else if (mod.compareTo("-") == 0) numGrade -= .3;
+		else numGrade = numGrade;
+		
+		if (l.compareTo("A") == 0) {
+			if ((numGrade += 4) > 4) numGrade = 4;
+		}
+		else if (l.compareTo("B") == 0) numGrade += 3;
+		else if (l.compareTo("C") == 0) numGrade += 2;
+		else if (l.compareTo("D") == 0) numGrade += 1;
+		else numGrade = 0;
+		
+		return String.valueOf(numGrade);
+		
+		
+	}
+
+	//String lgrade = "";
 
 }
--- PE05_05/src/GradeTester.java
+++ PE05_05/src/GradeTester.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class GradeTester {
 	
-	public static void main(Strins[] args) {
+	public static void main(String[] args) {
 		
 		Scanner in = new Scanner(System.in);
 		Grade grade = new Grade();
 		
 		System.out.println("Enter a letter grade:");
 		
-		System.out.println( grade.getNumericGrade( in.nextLine() ) );
+		System.out.println( grade.EgetNumericGrade( in.nextLine() ) );
 	}
-
+	
 }
