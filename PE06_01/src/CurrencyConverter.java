8512133ac1930ed9c86615dd566a99bcd72027b3
RBPEDIIIAL <tbarnabic1@gmail.com>
2018-11-30 08:33:57 -0500

    sdfsd

--- PE06_01/src/CurrencyConverter.java
+++ PE06_01/src/CurrencyConverter.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class CurrencyConverter {
 	
@@ -5,6 +6,25 @@ public class CurrencyConverter {
 		
 		Scanner in = new Scanner(System.in);
 		
+		boolean test = true;
+		String input;
+		while (test) {
+			
+			System.out.println("Enter USD Value:");
+			
+			input = in.nextLine();
+			
+			if ( input.equalsIgnoreCase("q") ) {
+				System.out.println("Terminated");
+				test = false;
+			}
+			else {
+				if (in.hasNextDouble()) {
+					System.out.println(in.nextDouble() * 0.88);
+				}
+				else System.out.println("Invalid Input");
+			}
+		}
 		
 	}
 
