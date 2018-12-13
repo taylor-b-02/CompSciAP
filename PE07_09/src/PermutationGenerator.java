b9f82d875c3d4ace8423cbbab24a21b7d702d72c
RBPEDIIIAL <tbarnabic1@gmail.com>
2018-12-12 22:15:50 -0500

    Finished PE07_09 like a ucking god

--- PE07_09/src/PermutationGenerator.java
+++ PE07_09/src/PermutationGenerator.java
@@ -1,8 +1,25 @@
+import java.util.Arrays;
+import java.util.Random;
 
 public class PermutationGenerator {
 	
 	public static void main(String[] args) {
 		
+		int[] valueArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		int[] permutationArray = new int[10];
+		int i = 0;
+		Random rand = new Random();
+		while (i < 10) {
+			int r = rand.nextInt(10);
+			
+			if (valueArray[r] != 0) {
+				permutationArray[i] = valueArray[r];
+				valueArray[r] = 0;
+				i ++;
+			}
+		}
+		
+		System.out.println(Arrays.toString(permutationArray));
 	}
 
 }
