2b062cfa9d769f149ac7d0e292922c03c189273d
RBPEDIIIAL <tbarnabic1@gmail.com>
2018-12-13 09:43:22 -0500

    Created PE07_10 and PE07_12
    Created but didn't finish

---
+++ PE07_10/src/TicTacToe.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+public class TicTacToe
+{
+   private String[][] board;
+   private static final int ROWS = 3;
+   private static final int COLUMNS = 3;
+
+
+   public TicTacToe()
+   {
+      board = new String[ROWS][COLUMNS];
+      // Fill with spaces
+      for (int i = 0; i < ROWS; i++)
+         for (int j = 0; j < COLUMNS; j++)
+            board[i][j] = " ";
+   }
+
+ 
+   public void set(int i, int j, String player)
+   {
+      if (board[i][j].equals(" "))
+         board[i][j] = player;
+   }
+
+
+   public String toString()
+   {
+      String r = "";
+      for (int i = 0; i < ROWS; i++)
+      {
+         r = r + "|";
+         for (int j = 0; j < COLUMNS; j++)         
+            r = r + board[i][j];
+         r = r + "|\n";
+      }
+      return r;
+   }
+   
+   
+   public String getWinner() {
+	   String w = "";
+	   
+	   for (int i = 0; i < ROWS; i++) {
+		   for (int j = 0; j < COLUMNS; j++) {
+			   if ((board[i][j].equals("x")) && board[i][j+1] && board[i][j+2]) w = "x";
+		   }
+	   }
+	   return w;
+   }
+}
